ccaacb8b5b908e9d40d662a2340e61c7a1f0a20c
Josias <87546094+Josyas@users.noreply.github.com>
2021-10-04 21:51:30 -0300

    Add files via upload

---
+++ Imc.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ImcPoo {
+    class Imc {
+
+        public double Altura;
+        public double Peso;
+        public string Nome;
+
+        public double Resultado() {
+            return Peso / (Altura * Altura);
+            
+        }
+
+        public override string ToString() {
+            return Nome + ","
+                + "o seu IMC: "
+                + Resultado().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ImcPoo {
+    class Program {
+        static void Main(string[] args) {
+
+            Imc imc = new Imc();
+
+            Console.WriteLine("|-----------------------|");
+            Console.WriteLine("|----Calculadora IMC----|");
+            Console.WriteLine("|-----------------------|");
+
+            Console.WriteLine();
+            Console.Write("Nome: ");
+            imc.Nome = Console.ReadLine();
+            Console.Write("Peso: ");
+            imc.Peso = double.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            imc.Altura = double.Parse(Console.ReadLine());
+            
+            
+
+            Console.WriteLine();
+            Console.WriteLine(imc);
+            if (imc.Resultado() < 18.5) {
+                Console.WriteLine("Abaixo do Peso!");
+            }
+            else if (imc.Resultado() < 24.9) {
+                Console.WriteLine("Peso idel, Parabéns!");
+            }
+            else if (imc.Resultado() < 29.9) {
+                Console.WriteLine("Levemente acima do peso!");
+            }
+            else if (imc.Resultado() < 34.9) {
+                Console.WriteLine("Obsidade grau 1!");
+            }
+            else if (imc.Resultado() < 39.9) {
+                Console.WriteLine("Obsidade grau 2, sereva!");
+            }
+            else {
+                Console.WriteLine("Obsidade 3, mórbida!");
+            }
+        }
+    }
+}
